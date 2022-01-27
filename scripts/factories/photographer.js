ebc85c412116908b97699e420db1b27aa3a4de6c
Adrien <47904625+Exeys@users.noreply.github.com>
2022-01-27 17:35:22 +0100

    import photographers + display infos

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,22 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const articleFocusable = document.createElement('div')
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("alt", name)
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const p = document.createElement('p');
+        p.innerHTML = city + ', ' + country + '<br>' + tagline + '<br>' + price + '€/jour';
+        article.appendChild(articleFocusable)
+        articleFocusable.appendChild(img);
+        articleFocusable.appendChild(h2);
+        article.appendChild(p);
         return (article);
     }
     return { name, picture, getUserCardDOM }
